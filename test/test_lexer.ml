634b6f1f8993bc7b5bd81cea93289858a4c22110
Brandon Tripp <brandongtripp@gmail.com>
2024-02-08 12:39:08 -0700

    WIP: Corrections to make alcotest run. Need to make pass and understand issue with no pass

--- test/test_lexer.ml
+++
@@ -1,7 +0,0 @@
-open Lexer
-
-let simple_test() = 
-    let input = "(+ 2 x)" in
-    let expected = [LeftParen; Symbol "+"; Number 2; Symbol "x"; RightParen] in 
-    let actual = Lexer.lex input in 
-    assert_equal expected actual
