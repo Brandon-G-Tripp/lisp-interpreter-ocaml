2562e6c5ea1856c7e9f802a7787a48f39dc99752
Brandon Tripp <brandongtripp@gmail.com>
2024-02-20 16:58:26 -0700

    initial basic tests for the lexer

--- lisp_interpreter/test/test_lexer.ml
+++ lisp_interpreter/test/test_lexer.ml
@@ -1,7 +1,23 @@
 open Lib
+open Alcotest
 
-let%test "_test simple lexer" = 
-  let lexbuf = Lexing.from_string "1" in
-  match Lexer.token lexbuf with
-  | "1" -> true
-  | _ -> false
+let test_simple_lexer () = 
+      let tokens = lex "1" in
+      Alcotest.(check (list token)) "Single number" tokens [Number 1]
+
+let test_composite_lexer () = 
+    let tokens = lex "(+ 2 3)" in
+    Alcotest.(check (list token))
+        "Expression with numbers and symbols" tokens
+        [LeftParen; Symbol "+"; Number 2; Number 3; RightParen]
+
+let tests = [
+    "Suite1", [
+        test_case "Simple lexer" `Quick test_simple_lexer;
+        test_case "Composite lexer" `Quick test_composite_lexer;
+    ];
+]
+
+let () = 
+    Alcotest.run "Test Suite" tests
+
